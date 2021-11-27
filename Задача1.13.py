2beb203f05c8f5e7eda3a83e3782f749ef95e718
Grishka3002 <grishapinchuk2013@gmail.com>
2021-11-27 01:59:36 -0500

    -

---
+++ Задача1.13.py
@@ -0,0 +1,10 @@
+#13.Напишите функцию Python, которая печатает первые n строк треугольника Паскаля.  
+a = int(input())
+
+def PrintPasTriangle(rows):
+    row = [1]
+    for i in range(rows):
+        print(row)
+        row = [sum(x) for x in zip([0]+row, row+[0])]
+        
+PrintPasTriangle(a)
