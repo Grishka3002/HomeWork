4cd8e38093cbc10effe2e2e65b15cf1388c5510d
Grishka3002 <grishapinchuk2013@gmail.com>
2021-12-06 18:45:59 -0500

    -

---
+++ семинар№1.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+
+url = "https://nplus1.ru/"
+a = []
+b = []
+linkd = []
+
+page = requests.get(url)
+text = page.text 
+
+
+soup = BeautifulSoup(text, "html.parser")
+for link in soup.findAll('a'):
+    a.append(link.get('href'))
+for i in a:
+    p = re.findall("/news.+", i)
+    b.append(p)
+for i in range(len(b)):
+    if len(b[i]) != 0:
+        linkd.append(b[i])
+
+
