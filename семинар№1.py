8343ce25211ec841e0aaae07aa9958b526b0a590
Grishka3002 <grishapinchuk2013@gmail.com>
2021-12-08 22:33:19 -0500

    -

--- семинар№1.py
+++ семинар№1.py
@@ -2,11 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+from time import sleep
 
 url = "https://nplus1.ru/"
 a = []
 b = []
 linkd = []
+time = []
 
 page = requests.get(url)
 text = page.text 
@@ -18,7 +20,14 @@ for link in soup.findAll('a'):
 for i in a:
     p = re.findall("/news.+", i)
     b.append(p)
-for i in range(len(b)):
-    if len(b[i]) != 0:
-        linkd.append(b[i])
-print(linkd)
+for i in b:
+    if len(i) != 0:
+        c = 'https://nplus1.ru'+''.join(i)
+        page = requests.get(c)
+        text = page.text
+        soup = BeautifulSoup(text, "lxml")
+        for i in soup.findAll('time'):
+            time.append(i)
+for i in time:
+    #p = re.findall("\d{2}\s\w{3}\.\s\d{4}", i)
+    print(i)
