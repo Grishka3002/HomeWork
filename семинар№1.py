a9e6dbbdcdf0dd58d6604e3308e4bdb6efe9b999
Grishka3002 <grishapinchuk2013@gmail.com>
2021-12-11 15:41:42 -0500

    -

--- семинар№1.py
+++ семинар№1.py
@@ -13,8 +13,8 @@ text = page.text
 soup = BeautifulSoup(text, "lxml")
 
 for a in soup.find_all('article', {"class":"item item-news item-news- _exist-image"}):
-    link = url + l('a')[0].get('href')
-    date = l('span')[0].get('title')
+    link = url + a('a')[0].get('href')
+    date = a('span')[0].get('title')
     page2 = requests.get(link)
     soup = BeautifulSoup(page2.text,"lxml")
     all_content = soup.find_all('article', {"class":"content"})
@@ -22,15 +22,15 @@ for a in soup.find_all('article', {"class":"item item-news item-news- _exist-ima
         dif = content('span', {"class":"difficult-value"})[0].text
         rubrics = content('p', {"class":"table"})[0].text
         title = content('h1')[0].text
-        texts = content('p',{"class":None})
-        author = texts[len(texts)-1].text
+        text = content('p',{"class":None})
+        author = text[len(text)-1].text
         text_container = content('p',{"class":None})
         texts = ""
         desc = None
         for i in range(0,len(text_container)-2):
-            texts += text_container[i].texts
-    res.append({'link':link, 'date':date, 'author':author, 'desc':desc, 'title':title, 'text':texts, 'rubric':rubrics.strip(), 'diff':[dif]})
+            texts += text_container[i].text
+    alls.append({'link':link, 'date':date, 'author':author, 'desc':desc, 'title':title, 'text':texts, 'rubric':rubrics.strip(), 'diff':[dif]})
     sleep(2)
-df = pandas.DataFrame(res)
+df = pd.DataFrame(alls)
 df.head()
-df.to_csv("Семинар1.csv", sep=";", encoding='utf-8', index=False)
+df.to_csv("Семинар1.csv", encoding='utf-8', index=False)
